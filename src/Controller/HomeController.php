7e36f3300a1aade6357af8945f77276f720e8353
Ivona Milankovic <ivonamilankovic@gmail.com>
2022-11-06 20:40:53 +0100

    home page

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
+{
+    /**
+     * @Route("/",name="app_home")
+     */
+    public function homePage():Response
+    {
+        return $this->render('home/home.html.twig');
+    }
+}
